0006b1eb4e731e1e8f0ebe6b9d7b6e382132ba6b
hak9292 <ak112997@gmail.com>
2022-03-31 11:04:28 -0400

    updated and finished category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,36 +13,43 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   // find one category by its `id` value
-  Category.fineOne({
+  Category.findOne(
+    {
     where: {
-      id: req.body.id,
+      id: req.params.id
     },
     // be sure to include its associated Products
     include: [Product]
   })
-  .then((category) => res.json(category))
-
+  .then((category) => res.json(`Retrieved Category ${req.params.id} successfully`));
 });
 
 router.post('/', (req, res) => {
   // create a new category
   Category.create(req.body)
-  .then((category) => res.json(category))
+  .then((category) => res.json(`Created Category ${req.body}`))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  Category.update(
-    {
-      
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
     }
-  )
+  })
+  .then((category) => res.json(`Updated Category ${req.params.id} to Category ${category}`));
 });
-
+// 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((category) => res.json(`Deleted Category ${req.params.id}`));
 });
 
 module.exports = router;
